8494b469bf9249d194f732c4ddf708ca8c4ee194
Milos Petrovic <mpetrovic290@gmail.com>
2019-12-04 21:16:10 +0100

    Business layer classes

---
+++ SMDiscover/BusinessLayer/CityBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class City
+    {
+        public City()
+        {
+            Country = new Country();
+        }
+
+        public string CityName { get; set; }
+        public Country Country { get; set; }
+    }
+}
---
+++ SMDiscover/BusinessLayer/RatingBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Rating
+    {
+        public int UserId { get; set; }
+        public int ShopId { get; set; }
+        public int Rate { get; set; }
+        public string Comment { get; set; }
+    }
+}
