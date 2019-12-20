0ea71863f7e0e10c52349989970af11f5f4381e8
Ilija Vujicic <ilijavujicic1998@gmail.com>
2019-12-20 19:13:04 +0100

    U potpunosti ispravan presentation layer: *Tri forme: -Forma za login, register, forgot password -Glavna forma aplikacije discover, about -Admin forma discover, editMall, editShop, editUser -I svi potrebni user control file-ovi

---
+++ SMDiscover/PresentationLayer/About.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace PresentationLayer
+{
+    partial class About
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.lblVanja = new System.Windows.Forms.Label();
+            this.lblLazar = new System.Windows.Forms.Label();
+            this.lblIlija = new System.Windows.Forms.Label();
+            this.lblMilos = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl31 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl21 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl41 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.pbMilos = new System.Windows.Forms.PictureBox();
+            this.pbIlija = new System.Windows.Forms.PictureBox();
+            this.pbLazar = new System.Windows.Forms.PictureBox();
+            this.pbVanja = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMilos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIlija)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLazar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVanja)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lblRegister.Location = new System.Drawing.Point(271, 38);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(225, 36);
+            this.lblRegister.TabIndex = 6;
+            this.lblRegister.Text = "Rick\'s Solutions";
+            // 
+            // lblVanja
+            // 
+            this.lblVanja.AutoSize = true;
+            this.lblVanja.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVanja.ForeColor = System.Drawing.Color.White;
+            this.lblVanja.Location = new System.Drawing.Point(13, 216);
+            this.lblVanja.Name = "lblVanja";
+            this.lblVanja.Size = new System.Drawing.Size(169, 22);
+            this.lblVanja.TabIndex = 16;
+            this.lblVanja.Text = "Vanja Stepanoski";
+            // 
+            // lblLazar
+            // 
+            this.lblLazar.AutoSize = true;
+            this.lblLazar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLazar.ForeColor = System.Drawing.Color.White;
+            this.lblLazar.Location = new System.Drawing.Point(13, 391);
+            this.lblLazar.Name = "lblLazar";
+            this.lblLazar.Size = new System.Drawing.Size(151, 22);
+            this.lblLazar.TabIndex = 17;
+            this.lblLazar.Text = "Lazar Starčević";
+            // 
+            // lblIlija
+            // 
+            this.lblIlija.AutoSize = true;
+            this.lblIlija.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIlija.ForeColor = System.Drawing.Color.White;
+            this.lblIlija.Location = new System.Drawing.Point(630, 216);
+            this.lblIlija.Name = "lblIlija";
+            this.lblIlija.Size = new System.Drawing.Size(102, 22);
+            this.lblIlija.TabIndex = 18;
+            this.lblIlija.Text = "Ilija Vujičić";
+            // 
+            // lblMilos
+            // 
+            this.lblMilos.AutoSize = true;
+            this.lblMilos.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMilos.ForeColor = System.Drawing.Color.White;
+            this.lblMilos.Location = new System.Drawing.Point(630, 391);
+            this.lblMilos.Name = "lblMilos";
+            this.lblMilos.Size = new System.Drawing.Size(133, 22);
+            this.lblMilos.TabIndex = 19;
+            this.lblMilos.Text = "Miloš Petrović";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lbl1.Location = new System.Drawing.Point(140, 169);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(174, 22);
+            this.lbl1.TabIndex = 20;
+            this.lbl1.Text = "----> Scrum Master";
+            // 
+            // lbl11
+            // 
+            this.lbl11.AutoSize = true;
+            this.lbl11.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl11.ForeColor = System.Drawing.Color.White;
+            this.lbl11.Location = new System.Drawing.Point(182, 191);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(73, 22);
+            this.lbl11.TabIndex = 21;
+            this.lbl11.Text = "9/2017";
+            // 
+            // lbl31
+            // 
+            this.lbl31.AutoSize = true;
+            this.lbl31.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl31.ForeColor = System.Drawing.Color.White;
+            this.lbl31.Location = new System.Drawing.Point(182, 299);
+            this.lbl31.Name = "lbl31";
+            this.lbl31.Size = new System.Drawing.Size(73, 22);
+            this.lbl31.TabIndex = 23;
+            this.lbl31.Text = "8/2017";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lbl3.Location = new System.Drawing.Point(140, 277);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(326, 22);
+            this.lbl3.TabIndex = 22;
+            this.lbl3.Text = "----> Database, Data Access Layer";
+            // 
+            // lbl21
+            // 
+            this.lbl21.AutoSize = true;
+            this.lbl21.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl21.ForeColor = System.Drawing.Color.White;
+            this.lbl21.Location = new System.Drawing.Point(336, 124);
+            this.lbl21.Name = "lbl21";
+            this.lbl21.Size = new System.Drawing.Size(84, 22);
+            this.lbl21.TabIndex = 25;
+            this.lbl21.Text = "45/2017";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lbl2.Location = new System.Drawing.Point(336, 102);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(292, 22);
+            this.lbl2.TabIndex = 24;
+            this.lbl2.Text = "Design, Presentation Layer <----";
+            // 
+            // lbl41
+            // 
+            this.lbl41.AutoSize = true;
+            this.lbl41.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl41.ForeColor = System.Drawing.Color.White;
+            this.lbl41.Location = new System.Drawing.Point(396, 365);
+            this.lbl41.Name = "lbl41";
+            this.lbl41.Size = new System.Drawing.Size(84, 22);
+            this.lbl41.TabIndex = 27;
+            this.lbl41.Text = "93/2017";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lbl4.Location = new System.Drawing.Point(396, 343);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(232, 22);
+            this.lbl4.TabIndex = 26;
+            this.lbl4.Text = "Business Logic Layer <----";
+            // 
+            // pbMilos
+            // 
+            this.pbMilos.Image = global::PresentationLayer.Properties.Resources.avatar_exemple1;
+            this.pbMilos.Location = new System.Drawing.Point(634, 277);
+            this.pbMilos.Name = "pbMilos";
+            this.pbMilos.Size = new System.Drawing.Size(117, 111);
+            this.pbMilos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMilos.TabIndex = 10;
+            this.pbMilos.TabStop = false;
+            // 
+            // pbIlija
+            // 
+            this.pbIlija.Image = global::PresentationLayer.Properties.Resources.avatar_exemple1;
+            this.pbIlija.Location = new System.Drawing.Point(634, 102);
+            this.pbIlija.Name = "pbIlija";
+            this.pbIlija.Size = new System.Drawing.Size(117, 111);
+            this.pbIlija.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbIlija.TabIndex = 9;
+            this.pbIlija.TabStop = false;
+            // 
+            // pbLazar
+            // 
+            this.pbLazar.Image = global::PresentationLayer.Properties.Resources.avatar_exemple1;
+            this.pbLazar.Location = new System.Drawing.Point(17, 277);
+            this.pbLazar.Name = "pbLazar";
+            this.pbLazar.Size = new System.Drawing.Size(117, 111);
+            this.pbLazar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLazar.TabIndex = 8;
+            this.pbLazar.TabStop = false;
+            // 
+            // pbVanja
+            // 
+            this.pbVanja.Image = global::PresentationLayer.Properties.Resources.avatar_exemple1;
+            this.pbVanja.Location = new System.Drawing.Point(17, 102);
+            this.pbVanja.Name = "pbVanja";
+            this.pbVanja.Size = new System.Drawing.Size(117, 111);
+            this.pbVanja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbVanja.TabIndex = 7;
+            this.pbVanja.TabStop = false;
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.Controls.Add(this.lbl41);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl21);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl31);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblMilos);
+            this.Controls.Add(this.lblIlija);
+            this.Controls.Add(this.lblLazar);
+            this.Controls.Add(this.lblVanja);
+            this.Controls.Add(this.pbMilos);
+            this.Controls.Add(this.pbIlija);
+            this.Controls.Add(this.pbLazar);
+            this.Controls.Add(this.pbVanja);
+            this.Controls.Add(this.lblRegister);
+            this.Name = "About";
+            this.Size = new System.Drawing.Size(771, 422);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMilos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIlija)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLazar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVanja)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.PictureBox pbVanja;
+        private System.Windows.Forms.PictureBox pbLazar;
+        private System.Windows.Forms.PictureBox pbIlija;
+        private System.Windows.Forms.PictureBox pbMilos;
+        private System.Windows.Forms.Label lblVanja;
+        private System.Windows.Forms.Label lblLazar;
+        private System.Windows.Forms.Label lblIlija;
+        private System.Windows.Forms.Label lblMilos;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lbl31;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl21;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl41;
+        private System.Windows.Forms.Label lbl4;
+    }
+}
---
+++ SMDiscover/PresentationLayer/Discover.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PresentationLayer
+{
+    partial class Discover
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            this.lblDiscover = new System.Windows.Forms.Label();
+            this.cbCountry = new System.Windows.Forms.ComboBox();
+            this.cbTown = new System.Windows.Forms.ComboBox();
+            this.lvDiscover = new System.Windows.Forms.ListView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDiscover
+            // 
+            this.lblDiscover.AutoSize = true;
+            this.lblDiscover.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscover.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.lblDiscover.Location = new System.Drawing.Point(127, 28);
+            this.lblDiscover.Name = "lblDiscover";
+            this.lblDiscover.Size = new System.Drawing.Size(488, 22);
+            this.lblDiscover.TabIndex = 34;
+            this.lblDiscover.Text = "Pleas select country and town you want, thank you.";
+            // 
+            // cbCountry
+            // 
+            this.cbCountry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbCountry.FormattingEnabled = true;
+            this.cbCountry.Location = new System.Drawing.Point(184, 66);
+            this.cbCountry.Name = "cbCountry";
+            this.cbCountry.Size = new System.Drawing.Size(180, 21);
+            this.cbCountry.TabIndex = 35;
+            // 
+            // cbTown
+            // 
+            this.cbTown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbTown.FormattingEnabled = true;
+            this.cbTown.Location = new System.Drawing.Point(396, 66);
+            this.cbTown.Name = "cbTown";
+            this.cbTown.Size = new System.Drawing.Size(180, 21);
+            this.cbTown.TabIndex = 36;
+            // 
+            // lvDiscover
+            // 
+            this.lvDiscover.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lvDiscover.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.lvDiscover.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvDiscover.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.lvDiscover.ForeColor = System.Drawing.Color.White;
+            this.lvDiscover.FullRowSelect = true;
+            this.lvDiscover.HideSelection = false;
+            listViewItem1.IndentCount = 124;
+            this.lvDiscover.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.lvDiscover.Location = new System.Drawing.Point(18, 115);
+            this.lvDiscover.Name = "lvDiscover";
+            this.lvDiscover.Size = new System.Drawing.Size(731, 247);
+            this.lvDiscover.TabIndex = 37;
+            this.lvDiscover.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.btnSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(197)))), ((int)(((byte)(130)))));
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Image = global::PresentationLayer.Properties.Resources.location_32px;
+            this.btnSearch.Location = new System.Drawing.Point(343, 368);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 33);
+            this.btnSearch.TabIndex = 38;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Discover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lvDiscover);
+            this.Controls.Add(this.cbTown);
+            this.Controls.Add(this.cbCountry);
+            this.Controls.Add(this.lblDiscover);
+            this.Name = "Discover";
+            this.Size = new System.Drawing.Size(771, 422);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDiscover;
+        private System.Windows.Forms.ComboBox cbCountry;
+        private System.Windows.Forms.ComboBox cbTown;
+        private System.Windows.Forms.ListView lvDiscover;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
