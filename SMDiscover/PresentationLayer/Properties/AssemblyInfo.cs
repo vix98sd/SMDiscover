f668df9de56b7ffac8a4024c43f06eb8abb5e1f5
Stepanoski Vanja <4830617-vix98sd@users.noreply.gitlab.com>
2019-12-20 19:11:20 +0000

    Revert "Merge branch 'developmentPresentationLayerIlija' into 'development'"
    This reverts merge request !10

--- SMDiscover/PresentationLayer/Properties/AssemblyInfo.cs
+++ SMDiscover/PresentationLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b13d6a56-1592-4f9d-bce1-286f9d238b83")]
+[assembly: Guid("9c5542ea-6693-41f6-bd0e-a00630c1de82")]
 
 // Version information for an assembly consists of the following four values:
 //
