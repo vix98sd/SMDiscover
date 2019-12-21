2b86df7a7de7dad3a9601c47674bf508114ee3d7
lazarstarco <lazarstarco@gmail.com>
2019-12-21 01:03:52 +0100

    Repositories filled (all but Shop and Shopping Mall (ask Vanja))

--- SMDiscover/DataLayer/RatingsRepository.cs
+++ SMDiscover/DataLayer/RatingsRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DataLayer.models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,76 @@ namespace DataLayer
 {
     public class RatingsRepository
     {
+        private string connectionString = "Data Source=Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SMDiscoverDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public List<Rating> GetAllRatings()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM RATINGS";
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                List<Rating> listToReturn = new List<Rating>();
+
+                while (sqlDataReader.Read())
+                {
+                    Rating rating = new Rating();
+
+                    rating.UserId = sqlDataReader.GetInt32(0);
+                    rating.ShopId = sqlDataReader.GetInt32(1);
+                    rating.Rate = sqlDataReader.GetInt32(2);
+                    rating.Comment = sqlDataReader.GetString(3);
+                }
+
+                return listToReturn;
+            }
+        }
+        public int InsertRating(Rating rating)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "INSERT INTO RATINGS (ID_USER, ID_SHOP, RATE, COMMENT) VALUES(" + string.Format(
+                    "'{0}', '{1}', '{2}', '{3}'", rating.UserId, rating.ShopId, rating.Rate, rating.Comment) + ")";
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public int UpdateRating(Rating rating)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "UPDATE RATINGS SET RATE = '" + rating.Rate +
+                    "', COMMENT = '" + rating.Comment + "' WHERE ID_USER = " + rating.UserId + " AND ID_SHOP" + rating.ShopId;
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteRating(int userID, int shopID)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "DELETE FROM RATINGS WHERE ID_USER = " + userID + " AND ID_SHOP = " + shopID;
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
     }
 }
