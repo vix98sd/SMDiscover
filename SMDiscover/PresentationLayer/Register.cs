bbdbbafaa1681b05ed7ada43d2f357d200e420ab
Milos Petrovic <milos.petrovic@traser-software.de>
2019-12-21 12:34:51 +0100

    Register form done!

--- SMDiscover/PresentationLayer/Register.cs
+++ SMDiscover/PresentationLayer/Register.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataLayer;
+using DataLayer.models;
 
 namespace PresentationLayer
 {
@@ -20,6 +22,45 @@ namespace PresentationLayer
         private void Register_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                if (tbName.Text == "" || tbSurname.Text == "" || tbEmail.Text == "" || tbUsername.Text == "" || tbPassword.Text == "" || tbConfirm.Text == "" || cbSecQuestion.Text == "" || tbAnswer.Text == "")
+                    throw new Exception("You must fill all the fields!");
+
+                if (tbUsername.Text.Length < 6)
+                    throw new Exception("Username must be at least 6 letters long!");
+
+                if (tbPassword.Text.Length < 8)
+                    throw new Exception("Password must be at least 8 characters long!");
+
+                if (tbPassword.Text != tbConfirm.Text)
+                    throw new Exception("Passwords must match up!");
+
+                User user = new User
+                {
+                    Name = tbName.Text,
+                    Surname = tbSurname.Text,
+                    Username = tbUsername.Text,
+                    Email = tbEmail.Text,
+                    Password = tbPassword.Text,
+                    SecretQuestion = cbSecQuestion.SelectedItem.ToString(),
+                    Answer = tbAnswer.Text
+                };
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void cbSecQuestion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbAnswer.Text = "";
         }
     }
 }
